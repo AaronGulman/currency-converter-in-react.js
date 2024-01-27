2062b9818318bce4169bdb7c04fd67f139f29526
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-01-27 10:37:58 -0500

    Import useEffect and useState from react. Add aplilayer.net API to app.js and create useEffect function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React, { useEffect, useState} from 'react';
 import './App.css';
 import CurrencyRow from './CurrencyRow';
 
+const BASE_URL = 'http://apilayer.net/api/live?access_key=268c2fed9a2e56c9bf97fa07ff916376&format'
+
+
 function App() {
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
  return (
   <>
     <h1>Convert</h1>
+    <CurrencyRow />
+    <div className='equals'>=</div>
     <CurrencyRow />
     </>
   );
